69e73a906fe441ee04482571cc015c10c8059eca
Faby121753 <m.f.vasconez@hotmail.com>
2023-07-24 16:06:13 -0500

    Agregar archivos de proyecto.

---
+++ ViewModels/NotesViewModelF_V.cs
@@ -0,0 +1,60 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Notes.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Notes.ViewModels
+{
+    internal class NotesViewModelF_V : IQueryAttributable
+    {
+        public ObservableCollection<ViewModels.NoteViewModelF_V> AllNotes { get; }
+        public ICommand NewCommand { get; }
+        public ICommand SelectNoteCommand { get; }
+
+        public NotesViewModelF_V()
+        {
+            AllNotes = new ObservableCollection<ViewModels.NoteViewModelF_V>(Models.Note.LoadAll().Select(n => new NoteViewModelF_V(n)));
+            NewCommand = new AsyncRelayCommand(NewNoteAsync);
+            SelectNoteCommand = new AsyncRelayCommand<ViewModels.NoteViewModelF_V>(SelectNoteAsync);
+        }
+
+        private async Task NewNoteAsync()
+        {
+            await Shell.Current.GoToAsync(nameof(Views.NotePage));
+        }
+
+        private async Task SelectNoteAsync(ViewModels.NoteViewModelF_V note)
+        {
+            if (note != null)
+                await Shell.Current.GoToAsync($"{nameof(Views.NotePage)}?load={note.Identifier}");
+        }
+
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("deleted"))
+            {
+                string noteId = query["deleted"].ToString();
+                NoteViewModelF_V matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note exists, delete it
+                if (matchedNote != null)
+                    AllNotes.Remove(matchedNote);
+            }
+            else if (query.ContainsKey("saved"))
+            {
+                string noteId = query["saved"].ToString();
+                NoteViewModelF_V matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note is found, update it
+                if (matchedNote != null)
+                {
+                    matchedNote.Reload();
+                    AllNotes.Move(AllNotes.IndexOf(matchedNote), 0);
+                }
+                // If note isn't found, it's new; add it.
+                else
+                    AllNotes.Insert(0, new NoteViewModelF_V(Models.Note.Load(noteId)));
+            }
+        }
+    }
+}
